ad303e341aae35e404c31488ff44c1a05dba0621
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-03-09 12:14:28 +0900

    doc: add docs

--- lib/src/rules/use_setstate_synchronously/use_setstate_synchronously_rule.dart
+++ lib/src/rules/use_setstate_synchronously/use_setstate_synchronously_rule.dart
@@ -4,6 +4,59 @@ import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:sangria/src/rules/use_setstate_synchronously/use_setstate_synchronously_visitor.dart';
 
+/// A `use_setstate_synchronously` rule that discourages the use of
+/// setState across asynchronous gaps within subclasses of State.
+///
+/// In async functions, the state of a widget may have been disposed across asynchronous gaps in a case when the user got back to previous screen. This leads to `setState() called after dispose()` error.
+/// Since widgets can be unmounted before a Future gets resolved, seeing if widgets are mounted is necessary before calling setState.
+///
+/// ### Example
+///
+/// #### ❌ BAD:
+///
+/// class _MyWidgetState extends State<MyWidget> {
+///   String message;
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     return Button(
+///       onPressed: () async {
+///         String fromSharedPreference = await getFromSharedPreference();
+///         // LINT: Avoid calling 'setState' across asynchronous gaps without seeing if the widget is mounted.
+///         setState(() {
+///           message = fromSharedPreference;
+///         });
+///       },
+///       child: Text(message),
+///     );
+///   }
+/// }
+/// ```
+///
+/// #### ✅ GOOD:
+///
+/// ```dart
+/// class _MyWidgetState extends State<MyWidget> {
+///   String message;
+
+///   @override
+///   Widget build(BuildContext context) {
+///     return Button(
+///       onPressed: () async {
+///         String fromSharedPreference = await getFromSharedPreference();
+///         if (mounted) {
+///           setState(() {
+///             message = fromSharedPreference;
+///           });
+///         }
+///       },
+///       child: Text(message),
+///     );
+///   }
+/// }
+/// ```
+///
+///
 class UseSetStateSynchronouslyLintRule extends DartLintRule {
   UseSetStateSynchronouslyLintRule()
     : super(
