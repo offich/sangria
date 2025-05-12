dfb0d28f8144b4cf6d87d8863f71d766df13275a
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-05-12 17:01:14 +0900

    feat: add example on use_widget_ref_synchronous_lint_rule

---
+++ example/lib/use_widget_ref_synchronously_lint_rule.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'use_widget_ref_synchronously_lint_rule.g.dart';
+
+class UseWidgetRefSynchronouslyLintRule extends HookConsumerWidget {
+  const UseWidgetRefSynchronouslyLintRule({super.key});
+
+  Future<void> incrementCounterWrapper(
+    BuildContext context,
+    WidgetRef ref,
+  ) async {
+    await ref
+        .read(asyncStateCounterNotifierProvider.notifier)
+        .incrementCounter();
+
+    if (context.mounted) {
+      await ref
+          .read(asyncStateCounterNotifierProvider.notifier)
+          .incrementCounter();
+    }
+
+    if (!context.mounted) {
+      await ref
+          .read(asyncStateCounterNotifierProvider.notifier)
+          .incrementCounter();
+      return;
+    }
+
+    if (!context.mounted) {
+      return;
+    }
+
+    await ref
+        .read(asyncStateCounterNotifierProvider.notifier)
+        .incrementCounter();
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(asyncStateCounterNotifierProvider);
+
+    return Column(
+      children: [
+        Text('counter: $counter'),
+        GestureDetector(
+          child: Text('increment'),
+          onTap: () async {
+            await ref
+                .read(asyncStateCounterNotifierProvider.notifier)
+                .incrementCounter();
+
+            if (context.mounted) {
+              await ref
+                  .read(asyncStateCounterNotifierProvider.notifier)
+                  .incrementCounter();
+            }
+
+            if (!context.mounted) {
+              await ref
+                  .read(asyncStateCounterNotifierProvider.notifier)
+                  .incrementCounter();
+              return;
+            }
+
+            if (!context.mounted) {
+              return;
+            }
+
+            await ref
+                .read(asyncStateCounterNotifierProvider.notifier)
+                .incrementCounter();
+          },
+        ),
+      ],
+    );
+  }
+}
+
+@riverpod
+class AsyncStateCounterNotifier extends _$AsyncStateCounterNotifier {
+  @override
+  int build() {
+    return 0;
+  }
+
+  Future<void> incrementCounter() {
+    return Future.delayed(const Duration(seconds: 2), () => state++);
+  }
+}
