ec7bdf34f5fb17d579729977dad8ff3b109444ed
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-05-12 17:00:55 +0900

    feat: impl use_widget_ref_synchronously rule

--- lib/sangria_lints.dart
+++ lib/sangria_lints.dart
@@ -2,6 +2,7 @@ import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:sangria_lints/src/rules/avoid_empty_container/avoid_empty_container_lint_rule.dart';
 import 'package:sangria_lints/src/rules/no_disabled_tests/no_disabled_tests_lint_rule.dart';
 import 'package:sangria_lints/src/rules/use_setstate_synchronously/use_setstate_synchronously_rule.dart';
+import 'package:sangria_lints/src/rules/use_widget_ref_synchronously/use_widget_ref_synchronous_lint_rule.dart';
 
 /// Returns the Sangria Plugin instance.
 PluginBase createPlugin() => _SangriaLints();
@@ -11,6 +12,7 @@ class _SangriaLints extends PluginBase {
     UseSetStateSynchronouslyLintRule(),
     AvoidEmptyContainerLintRule(),
     NoDisabledTestsLintRule(),
+    UseWidgetRefSynchronouslyLintRule(),
   ];
 
   @override
